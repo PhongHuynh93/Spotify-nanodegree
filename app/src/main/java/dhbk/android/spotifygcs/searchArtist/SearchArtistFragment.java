cb417ddb715dec79583fddebb79d37d63c0e53b2
Phong <p7157568@gmail.com>
2016-07-14 20:23:13 +0700

    feat - add color to empty background

--- app/src/main/java/dhbk/android/spotifygcs/searchArtist/SearchArtistFragment.java
+++ app/src/main/java/dhbk/android/spotifygcs/searchArtist/SearchArtistFragment.java
@@ -23,7 +23,7 @@ public class SearchArtistFragment extends Fragment implements SearchArtistContra
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_artist, container, false);
+        return inflater.inflate(R.layout.fragment_search_artist_emtpy, container, false);
     }
 
     // set presenter for this view
