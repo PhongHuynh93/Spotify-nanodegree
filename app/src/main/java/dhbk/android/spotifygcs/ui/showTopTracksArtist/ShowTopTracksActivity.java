07f59bed2d25e9e1347a510e3dac2d591e61b9d1
Phong <p7157568@gmail.com>
2016-07-21 13:49:50 +0700

    feat -add class

--- app/src/main/java/dhbk/android/spotifygcs/ui/showTopTracksArtist/ShowTopTracksActivity.java
+++ app/src/main/java/dhbk/android/spotifygcs/ui/showTopTracksArtist/ShowTopTracksActivity.java
@@ -2,10 +2,13 @@ package dhbk.android.spotifygcs.ui.showTopTracksArtist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.transition.Transition;
+import android.view.View;
 
 import dhbk.android.spotifygcs.BaseActivity;
 import dhbk.android.spotifygcs.R;
 import dhbk.android.spotifygcs.util.ActivityUtils;
+import dhbk.android.spotifygcs.util.AnimUtils;
 import dhbk.android.spotifygcs.util.Constant;
 
 public class ShowTopTracksActivity extends BaseActivity {
@@ -51,6 +54,31 @@ public class ShowTopTracksActivity extends BaseActivity {
 
         // Create the presenter
         mShowTopTracksPresenter = new ShowTopTracksPresenter(showTopTracksFragment);
+
+        getWindow().getSharedElementReturnTransition().addListener(shotReturnHomeListener);
     }
+
+
+    private Transition.TransitionListener shotReturnHomeListener =
+            new AnimUtils.TransitionListenerAdapter() {
+                @Override
+                public void onTransitionStart(Transition transition) {
+                    super.onTransitionStart(transition);
+//                    // hide the fab as for some reason it jumps position??  TODO work out why
+//                    fab.setVisibility(View.INVISIBLE);
+                    // fade out the "toolbar" & list as we don't want them to be visible during return
+                    // animation
+                    back.animate()
+                            .alpha(0f)
+                            .setDuration(100)
+                            .setInterpolator(getLinearOutSlowInInterpolator(DribbbleShot.this));
+                    imageView.setElevation(1f);
+                    back.setElevation(0f);
+                    commentsList.animate()
+                            .alpha(0f)
+                            .setDuration(50)
+                            .setInterpolator(getLinearOutSlowInInterpolator(DribbbleShot.this));
+                }
+            };
 
 }
